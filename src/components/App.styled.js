e743a9c5ab219d827a4ac85d0835dc4e976e11ad
AnnaFutryk <anna.futryk@gmail.com>
2023-07-06 17:10:10 +0300

    Update App.styled.js

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding: 40px;
-  margin: 0 auto;
-  background-color: lightblue;
 `;
